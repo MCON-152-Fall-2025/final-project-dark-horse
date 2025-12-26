54b688f3c9be0325678119e586234ce8f49d4124
DJCoder311 <166066300+DJCoder311@users.noreply.github.com>
2025-12-25 20:36:36 -0500

    Created TagBadgeDecorator
    This class just wraps another recipeview interface and adds a new method returning tag badges of the recipe

---
+++ recipeshare/src/main/java/com/mcon152/recipeshare/domain/TagBadgeDecorator.java
@@ -0,0 +1,46 @@
+package com.mcon152.recipeshare.domain;
+
+import com.mcon152.recipeshare.domain.views.RecipeView;
+
+import java.util.Set;
+
+public class TagBadgeDecorator implements RecipeView{
+
+    private RecipeView wrappedView;
+
+    public TagBadgeDecorator(RecipeView wrappedView){
+        this.wrappedView = wrappedView;
+    }
+
+    @Override
+    public Recipe getRecipe() {
+        return wrappedView.getRecipe();
+    }
+
+    @Override
+    public String getDisplayName() {
+        return wrappedView.getDisplayName();
+    }
+
+    @Override
+    public String getDescription() {
+        return wrappedView.getDescription();
+    }
+
+    @Override
+    public String getIngredients() {
+        return wrappedView.getIngredients();
+    }
+
+    public String getTagBadges(){
+        StringBuilder badges = new StringBuilder();
+        Set<Tag> tags = wrappedView.getRecipe().getTags();
+        if(tags == null){
+            return "";
+        }
+        for(Tag tag: tags){
+            badges.append("[").append(tag).append("]");
+        }
+        return badges.toString();
+    }
+}
